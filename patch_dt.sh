03c790290ffa9ea820f5c6d7852256c47113ffdd
mustafa <118094847+mustafa-dgaf@users.noreply.github.com>
2025-07-20 14:38:01 +0300

    Delete patch_dt.sh

--- patch_dt.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-find device -name "lineage_*.mk" -exec sed -i 's|$(call inherit-product, vendor/lineage/config|#$(call inherit-product, vendor/lineage/config|g' {} \;
-# Samsung specific
-rm -rf hardware/samsung/doze
-rm -rf hardware/samsung/AdvancedDisplay
