bdc1fe20645ffbca0b0aeed030a95fc783bd129a
mustafa <118094847+mustafa-dgaf@users.noreply.github.com>
2025-07-20 14:36:12 +0300

    Update patch_dt.sh

--- patch_dt.sh
+++ patch_dt.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-
 find device -name "lineage_*.mk" -exec sed -i 's|$(call inherit-product, vendor/lineage/config|#$(call inherit-product, vendor/lineage/config|g' {} \;
-find device -name "*.mk" -exec sed -i 's|include device/lineage|#include device/lineage|g' {} \;
 # Samsung specific
 rm -rf hardware/samsung/doze
 rm -rf hardware/samsung/AdvancedDisplay
